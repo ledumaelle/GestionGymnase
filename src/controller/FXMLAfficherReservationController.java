2d6a38a4e99f9cf050e94b5201e11a2355ebe820
kamicasis <>
2018-11-26 09:10:20 +0100

    AfficherReservationController vider la gridPane

---
+++ src/controller/FXMLAfficherReservationController.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import model.GestionReservationBdD;
+import model.GestionSalleBdD;
+import model.Planning;
+import model.Salle;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rabelais
+ */
+public class FXMLAfficherReservationController implements Initializable
+{
+    @FXML
+    private ComboBox cmbSalle; 
+    @FXML
+    private DatePicker dateReservation;
+    @FXML
+    private GridPane gridReservation; 
+    
+    
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        ObservableList<Salle> lesSalles =  GestionSalleBdD.getSalle();
+        cmbSalle.setItems(lesSalles);     
+    } 
+    
+    public void handleSelectionSalle()
+    {
+        int i,j,k;
+        for(i=1;i<14;i++)
+        {
+            for(j=1;j<8;j++)
+            {
+                for(k=0;k<gridReservation.getChildren().size();k++)                  
+                {  
+                    Node child = gridReservation.getChildren().get(k);
+                    if(GridPane.getColumnIndex(child) != null && GridPane.getRowIndex(child) != null)
+                    {
+                        if(GridPane.getColumnIndex(child) == j && GridPane.getRowIndex(child)== i)
+                        {
+                            gridReservation.getChildren().remove(k);
+                        }
+                    }    
+                }
+            } 
+        }
+    }
+    
+    public void handleSelectionDate()
+    {
+        handleSelectionSalle();
+        if (cmbSalle.getSelectionModel().getSelectedItem() != null && dateReservation.getValue() != null ) 
+        {
+            Salle UneSalle = (Salle)(cmbSalle.getSelectionModel().getSelectedItem());
+            LocalDate UneDate = dateReservation.getValue();
+            int i,j,k;
+            int l=1;
+            int UnHoraire=0;
+            int UnJour=0;
+            String LAssoc="";
+            ObservableList<Planning> lePlanning= GestionReservationBdD.getPlanning(UneSalle,UneDate);
+            if (!(lePlanning.isEmpty()))
+            {
+                for(i=8;i<21;i++)
+                {
+                    for (j=1;j<8;j++)
+                    {
+                        for (k=0;k<lePlanning.size();k++)
+                        {
+                            UnHoraire = lePlanning.get(k).getUnHoraire();
+                            UnJour = lePlanning.get(k).getUnJour();
+                            LAssoc = lePlanning.get(k).getRefAssociation();
+                            if(UnHoraire == i && UnJour == j)
+                            {
+                                break;
+                            }
+                        }
+                        if (UnHoraire == i && UnJour == j)
+                        {
+                            if (LAssoc != null)
+                            {
+                                Label lblCase = new Label();
+                                lblCase.setText(LAssoc);
+                                lblCase.setStyle("-fx-text-fill: red;");
+                                GridPane.setConstraints(lblCase,j,l,1,1,HPos.CENTER,VPos.CENTER);
+                                gridReservation.getChildren().addAll(lblCase);
+                            }
+                        }
+                    }
+                    l++;
+                }  
+            }
+            
+        }
+        
+        //plusDays
+        //menosDays
+    }
+    
+}
